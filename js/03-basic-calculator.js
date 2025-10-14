017841d708d79ee9ba5b8f093228107364262445
asdfsdfdsfsd <asdfsdfsa@example.com>
2025-10-14 14:28:07 -0700

    upd

--- js/03-basic-calculator.js
+++ js/03-basic-calculator.js
@@ -1,5 +1,13 @@
 // ADD A FUNCTION CALLED CALCULATE
 
+function calculate() {
+    let x = parseFloat(prompt("Pick a first number"))
+    let y = parseFloat(prompt("Pick a second number"))
+    let operation = prompt("What kind of operation do you want to perform; add, subtract, divide, or multiply?")
+    console.log(x, y)
+}
+calculate()
+
 // COLLECT FIRST NUMBER FROM USER
 
 // COLLECT SECOND NUMBER FROM USER
